1dbe18c5698baf0dd0782cf72bf196d6b8f5e514
Max <maxduma16@gmail.com>
2021-01-18 17:59:10 +0200

    add dynamic modal

--- dist/script.js
+++ dist/script.js
@@ -150,6 +150,58 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
 
 Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-toggle="modal"]').modal();
 
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.createModal = function ({
+  text,
+  btns
+} = {}) {
+  for (let i = 0; i < this.length; i++) {
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+    const buttons = [];
+
+    for (let j = 0; j < btns.count; j++) {
+      let btn = document.createElement('button');
+      btn.classList.add('btn', ...btns.settings[j][1]);
+      btn.textContent = btns.settings[j][0];
+
+      if (btn.settings[j][2]) {
+        btn.setAttribute('data-close', 'true');
+      }
+
+      if (btns.settings[j][3] && typeof btns.settings[j][3] === 'function') {
+        btn.addEventListener('click', btns.settings[j][3]);
+      }
+
+      buttons.push(btn);
+    }
+
+    modal.innerHTML = `
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <button class="close" data-close>
+                <span>&times;</span>
+            </button>
+            <div class="modal-header">
+                <div class="modal-title">${text.title}</div>
+            </div>
+            <div class="modal-body">
+                ${text.body}
+            </div>
+              <div class="modal-footer">
+                <button class="btn btn-danger" data-close>Close</button>
+                <button class="btn btn-success">Save changes</button>
+              </div>
+            </div>
+        </div>
+        `;
+    modal.querySelector("modal-footer").append(...buttons);
+    document.body.appendChild(modal);
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).modal();
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i].getAttribute('data-target')).fadeIn(500);
+  }
+};
+
 /***/ }),
 
 /***/ "./src/js/lib/core.js":
